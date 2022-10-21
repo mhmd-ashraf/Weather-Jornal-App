c1ab5ee0ecadb9240b6dab57b1d15f4f7bdd9f72
mhmd-ashraf <77515422+mhmd-ashraf@users.noreply.github.com>
2022-10-21 12:52:13 +0200

    Add files via upload
    update the date

--- website/app.js
+++ website/app.js
@@ -8,7 +8,7 @@ const ApiKey = ",&appid=b0cf3394c0a327f9e0e50819defcf645&units=imperial";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + '-' + d.getDate() + '-' + d.getFullYear();
+let newDate = d.getMonth()+1 + '/' + d.getDate() + '/' + d.getFullYear();
 
 // Event listener to add function to existing HTML DOM element
 const btn = document.getElementById('generate');
